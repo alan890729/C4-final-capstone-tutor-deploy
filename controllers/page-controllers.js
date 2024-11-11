4e2267d04121efcfe4ba8a467e8e4675c8b8a587
alan890729 <alan890729@gmail.com>
2024-11-12 05:30:53 +0800

    feat: add paginations on tutors and rankings page

--- controllers/page-controllers.js
+++ controllers/page-controllers.js
@@ -1,49 +1,67 @@
 const { User, Student, Reservation, Sequelize } = require('../models')
+const pagination = require('../helpers/pagination-helpers')
 
 const pageControllers = {
-  getTutorsAndRankings (req, res, next) {
-    return Promise.all([
-      User.findAll({
-        attributes: { exclude: ['password'] },
-        where: { status: 'teacher' }
-      }),
-      Reservation.findAll({
-        attributes: [
-          'studentId',
-          [Sequelize.fn('SUM', Sequelize.col('duration_hours')), 'totalLearningTime']
-        ],
-        where: { isExpired: true },
-        include: [
-          {
-            model: Student,
-            attributes: ['userId'],
-            include: [
-              {
-                model: User,
-                attributes: ['name', 'avatar']
-              }
-            ]
-          }
-        ],
-        group: 'studentId',
-        order: [['totalLearningTime', 'DESC']],
-        limit: 10
-      })
-    ])
-      .then(([usersWithTeacherStatus, studentRankings]) => {
-        studentRankings = studentRankings.map(r => r.toJSON())
-        usersWithTeacherStatus = usersWithTeacherStatus.map(u => u.toJSON())
-
-        const learingTimes = []
-        studentRankings.forEach(r => {
-          if (!learingTimes.includes(r.totalLearningTime)) {
-            learingTimes.push(r.totalLearningTime)
-          }
-          r.ranking = learingTimes.length
+  async getTutorsAndRankings (req, res, next) {
+    try {
+      const RECORDS_PER_PAGE = 9
+      const currentPage = Number(req.query.page) || 1
+      const limit = Number(req.query.limit) || RECORDS_PER_PAGE
+      const offset = (currentPage - 1) * limit
+      let [
+        { count: amountOfTeachers, rows: usersWithTeacherStatus },
+        studentRankings
+      ] = await Promise.all([
+        User.findAndCountAll({
+          attributes: { exclude: ['password'] },
+          where: { status: 'teacher' },
+          limit,
+          offset
+        }),
+        Reservation.findAll({
+          attributes: [
+            'studentId',
+            [Sequelize.fn('SUM', Sequelize.col('duration_hours')), 'totalLearningTime']
+          ],
+          where: { isExpired: true },
+          include: [
+            {
+              model: Student,
+              attributes: ['userId'],
+              include: [
+                {
+                  model: User,
+                  attributes: ['name', 'avatar']
+                }
+              ]
+            }
+          ],
+          group: 'studentId',
+          order: [['totalLearningTime', 'DESC']],
+          limit: 10
         })
+      ])
+      studentRankings = studentRankings.map(r => r.toJSON())
+      usersWithTeacherStatus = usersWithTeacherStatus.map(u => u.toJSON())
 
-        return res.render('tutor/tutors-and-rankings', { usersWithTeacherStatus, studentRankings })
+      const paginators = pagination.generatePaginatorForRender(amountOfTeachers, currentPage, limit)
+
+      const learingTimes = []
+      studentRankings.forEach(r => {
+        if (!learingTimes.includes(r.totalLearningTime)) {
+          learingTimes.push(r.totalLearningTime)
+        }
+        r.ranking = learingTimes.length
       })
+
+      return res.render('tutor/tutors-and-rankings', {
+        usersWithTeacherStatus,
+        studentRankings,
+        paginators
+      })
+    } catch (err) {
+      return next(err)
+    }
   }
 }
 
